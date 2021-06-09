8085d1e1b95d13a79d787e42c4b183915b7866dd
RaneemAbdulazez <raneem97su@gmail.com>
2021-06-10 02:33:43 +0300

    done

--- data_base.py
+++ data_base.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3.dbapi2 import Cursor, connect
+from encryption import *
 # from tkinter import *
 # from PIL import ImageTk, Image
 
@@ -13,35 +14,41 @@ def drop_table():
     # # commit changes
     c.execute('DROP TABLE information')
     # create table
+    # full_name,age,cronic_diseas,gender,blood_type,image_date,result,img
     c.execute("""CREATE TABLE information ( 
         full_name text,
-        image_date date,
-        age integer,
-        sex text,
-        blod_type text,
-        cronic_diseas text
+        age text,
+        cronic_diseas text,
+        gender text,
+        blood_type text,
+        image_date text,
+        result text,
+        img BLOB
     )
      """)
     conn.commit()
 
-    # # close connection 
-    # conn.close()
 
-    # insert into table 
-
-def save_in_db(full_name,age,cronic_diseas,gender,blood_type,image_date,result,img):
+def save_in_db(full_name,age,cronic_diseas,gender,blood_type,image_date,result,img_data):
     conn=sqlite3.connect('paitent_database.db')
     # # # create Database 
     c=conn.cursor()  # sends command
-    c.execute("INSERT INTO information VALUES(:full_name,:image_date,:age,:sex,:blod_type,:cronic_diseas)",
+    # with open(img,rb)as f:
+    #     img_data=f.read()
+
+    c.execute("INSERT INTO information VALUES(:full_name,:age,:cronic_diseas,:gender,:blood_type,:image_date,:result,:img_data)",
 
     {
-        'full_name':full_name,
-        'image_date':image_date,
-        'age':age,
-        'gender':gender,
-        'blod_type':blod_type,
-        'cronic_diseas':cronic_diseas
+       'full_name':full_name,
+       'age':age,
+       'cronic_diseas':cronic_diseas,
+       'gender':gender,
+       'blood_type':blood_type,
+       'image_date':image_date,
+       'result':result,
+       'img_data':img_data,
+
+
     }
 
     )
@@ -57,29 +64,42 @@ def save_in_db(full_name,age,cronic_diseas,gender,blood_type,image_date,result,i
 
 
 def load_from_db():
+
     # query data
-    c.execute("SELECT * ,oid FROM information")
+    y=c.execute("SELECT * ,oid FROM information")
     recoreds=c.fetchall()
-    for rec in recoreds:
-        print(rec)  
+    decrypt_blocks(recoreds,encrpted_data[1])
+
+    for i in recoreds:
+        image=i[7]
+        name=i[0]
+        # print(image)
+        with open(f'{i[0]}','wb') as f:
+            f.write(image)
+        
+  
 
 def query_by_name(full_name_p):
     conn=sqlite3.connect('paitent_database.db')
-    # # # create Database 
-    c=conn.cursor()  # sends command
-    # c.execute("SELECT * ,oid FROM information")
     x=c.execute("SELECT * from information WHERE full_name="+'\''+full_name_p+'\'')
-    print(x.fetchall())
 
 
-
-# query_by_name("Raneem")
 
 drop_table()
-save_info_in_db('Raneem','10/5/2021',23,'F','B+','none')
-save_info_in_db('Sami','10/5/2021',23,'F','B+','none')
+with open('assets/personal_info.png','rb')as f:
+    img_data=f.read()    
 
-# save_info_in_db('Sami','10/5/2021',23,'F','none')
-# save_info_in_db('Rami','10/5/2021',23,'F','none')
-# Load_data()
+encrpted_data=encrypt_blocks('Raneem',23,'he is good','F','B+','10/5/2015')
+# save_in_db(encrpted_data[0][0],encrpted_data[0][1],encrpted_data[0][2],encrpted_data[0][3],encrpted_data[0][4],encrpted_data[0][5],'postive',img_data)
+
+
+
+# for i in encrpted_data:
+#     print(i)
+
+load_from_db()
 query_by_name("Raneem")
+
+
+
+
--- encryption.py
+++ encryption.py
@@ -1,6 +1,11 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 import base64
+from  decouple import config
+# from data_base import save_in_db
+
+KEY=config('KEY')
+print(KEY)
 
 
 def encrypt_blocks(name,age,Chronic_diseases,gender,blood_type,date):
@@ -10,7 +15,7 @@ def encrypt_blocks(name,age,Chronic_diseases,gender,blood_type,date):
     info=info +[name,age,Chronic_diseases,gender,blood_type,date]
     padding="*"
     block_size= 16
-    key=Random.new().read(16)
+    key=KEY
     iv=Random.new().read(16)
     print(iv)
     print(len(iv))
@@ -19,17 +24,17 @@ def encrypt_blocks(name,age,Chronic_diseases,gender,blood_type,date):
         E=AES.new(key,AES.MODE_CBC,iv)
         ciphertext=base64.b64encode(iv+E.encrypt(q(i).encode('ascii')))
         encrypt_info.append(ciphertext)
-    return [encrypt_info,key]
+    return encrypt_info
     
      
 
 
-def decrypt_blocks(encrypt_info,key):
+def decrypt_blocks(encrypt_info):
     decrypt_info=[]
     for i in encrypt_info:
         iv=base64.b64decode(i)[:16]
         i_e=base64.b64decode(i)[16:]
-        d=AES.new(key,AES.MODE_CBC,iv)
+        d=AES.new(KEY,AES.MODE_CBC,iv)
         decrypt_i=d.decrypt(i_e).decode('ascii')
         d_i=decrypt_i.rstrip("*")
         decrypt_info.append(d_i)
@@ -38,16 +43,7 @@ def decrypt_blocks(encrypt_info,key):
   
 
 
-
-def encrypt_image(image):
-    pass
-def decrypt_image(image):
-    pass
-
-   
-
-
 if __name__ == "__main__":
     a = encrypt_blocks('Noura',25,'None','female', 'A+','9/6/2021')
     print(a)
-    print(decrypt_blocks(a[0],a[1]))
+    # print(decrypt_blocks(a[0][]))
